ddda06193683de172a9605c2a0b21971d0d23bcb
fesstor <emelya1993@rambler.ru>
2015-01-29 18:28:20 +0300

    Remove connections to mollie API

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -87,7 +87,7 @@ require 'webmock/rspec'
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/vcr_cassettes'
   config.hook_into :webmock
-  config.allow_http_connections_when_no_cassette = true
+  config.ignore_localhost = true
 end
 
 ENV['MOLLIE_TEST_API_KEY'] = 'test_pw5ZHNihuiFKefzBwZVwAdKXt5C4Xe'
